394ab50e3999bafcccaf835847a69f01f7131350
mohammad salah <123891711+mohammadsalah02@users.noreply.github.com>
2024-08-27 13:37:48 +0300

    m
    m

---
+++ TaskFolwForBegginersAndIntermedietes/ViewController/src/view/EmployeeDetailProccessBean.java
@@ -0,0 +1,19 @@
+package view;
+
+public class EmployeeDetailProccessBean {
+    Number departmentIdParam = null;
+
+    public void setDepartmentIdParam(Number departmentIdParam) {
+        this.departmentIdParam = departmentIdParam;
+    }
+
+    public Number getDepartmentIdParam() {
+        return departmentIdParam;
+    }
+
+    public EmployeeDetailProccessBean() {
+        super();
+    }
+    
+    
+}
